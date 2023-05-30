536ffcb8839f0b062cbad7accec672314c5903e2
monica-cervantes <46661858+monica-cervantes@users.noreply.github.com>
2023-05-30 09:53:50 +0800

    enhance scrollToTop button

--- src/app/components/scrolltotop.js
+++ src/app/components/scrolltotop.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 import styles from '../styles/global.module.scss'
 import { animateScroll as scroll } from 'react-scroll'
 
 const ScrollToTop = () => {
+  const [showButton, setShowButton] = useState(false);
+
   const scrollToTop = () => {
     scroll.scrollToTop();
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <button className={styles.scroll_to_top} onClick={scrollToTop}>
+    <button 
+      className={`${styles.scroll_to_top} ${showButton ? styles.show : ''}`} 
+      onClick={scrollToTop}
+    >
       <FontAwesomeIcon icon={faArrowUp} />
     </button>
   )
